numerique-gouv/impress

✅(frontend) adapt all tests related to the new header
Since we no longer use an editable div but an input, we must
modify the tests accordingly

src/frontend/apps/e2e/__tests__/app-impress/common.ts
@@ -40,12 +40,19 @@ export const createDoc = async (
       })
       .click();
 
-    await page.getByRole('heading', { name: 'Untitled document' }).click();
-    await page.keyboard.type(randomDocs[i]);
-    await page.getByText('Created at ').click();
+    const input = page.getByRole('textbox', { name: 'doc title input' });
+    await input.click();
+    await input.fill(randomDocs[i]);
+    await input.blur();
   }
 
   return randomDocs;
+};
+
+export const verifyDocName = async (page: Page, docName: string) => {
+  const input = page.getByRole('textbox', { name: 'doc title input' });
+  await expect(input).toBeVisible();
+  await expect(input).toHaveValue(docName);
 };
 
 export const addNewMember = async (

suitenumerique/docs

src/frontend/apps/e2e/__tests__/app-impress/common.ts
@@ -40,12 +40,19 @@ export const createDoc = async (
       })
       .click();
 
-    await page.getByRole('heading', { name: 'Untitled document' }).click();
-    await page.keyboard.type(randomDocs[i], { delay: 100 });
-    await page.getByText('Created at ').click();
+    const input = page.getByRole('textbox', { name: 'doc title input' });
+    await input.click();
+    await input.fill(randomDocs[i]);
+    await input.blur();
   }
 
   return randomDocs;
+};
+
+export const verifyDocName = async (page: Page, docName: string) => {
+  const input = page.getByRole('textbox', { name: 'doc title input' });
+  await expect(input).toBeVisible();
+  await expect(input).toHaveText(docName);
 };
 
 export const addNewMember = async (