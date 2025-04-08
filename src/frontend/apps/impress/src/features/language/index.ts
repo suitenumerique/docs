fee1cf1274362f9d816fd3c442e17d1464158c7b
rvveber <weber@b1-systems.de>
2025-04-08 16:09:30 +0200

    ✨(frontend) Adds customization for translations
    Part of customization PoC
    
    Signed-off-by: Robin Weber <weber@b1-systems.de>

--- src/frontend/apps/impress/src/features/language/index.ts
+++ src/frontend/apps/impress/src/features/language/index.ts
@@ -1,2 +1,4 @@
-export * from './hooks/useLanguageSynchronizer';
-export * from './LanguagePicker';
+export * from './hooks';
+export * from './api';
+export * from './components';
+export * from './utils';
