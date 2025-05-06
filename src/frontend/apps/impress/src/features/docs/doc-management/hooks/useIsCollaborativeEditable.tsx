861be2ec7701760e730d848ca68faf0bedf4e8b8
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-05-06 14:25:10 +0200

    🛂(frontend) block edition to not connected users
    If an editor is working on a shared document but
    is not connected to the collaborative server
    we are now blocking the edition.
    It is to avoid none connected users to
    overwrite the document with connected
    users.

---
+++ src/frontend/apps/impress/src/features/docs/doc-management/hooks/useIsCollaborativeEditable.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import { useProviderStore } from '../stores';
+import { Doc, LinkReach } from '../types';
+
+export const useIsCollaborativeEditable = (doc: Doc) => {
+  const { isConnected } = useProviderStore();
+
+  const docIsPublic = doc.link_reach === LinkReach.PUBLIC;
+  const docIsAuth = doc.link_reach === LinkReach.AUTHENTICATED;
+  const docHasMember = doc.nb_accesses_direct > 1;
+  const isShared = docIsPublic || docIsAuth || docHasMember;
+  const [isEditable, setIsEditable] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
+
+  /**
+   * Connection can take a few seconds
+   */
+  useEffect(() => {
+    const _isEditable = isConnected || !isShared;
+
+    if (_isEditable) {
+      setIsEditable(true);
+      setIsLoading(false);
+      return;
+    }
+
+    const timer = setTimeout(
+      () => {
+        setIsEditable(false);
+        setIsLoading(false);
+      },
+      isLoading ? 5000 : 0,
+    );
+
+    return () => clearTimeout(timer);
+  }, [isConnected, isLoading, isShared]);
+
+  return {
+    isEditable,
+    isLoading,
+  };
+};
