822e9ad22d63b887c8aa545d89b89ef91c242d0c
Clément Di Domenico <clement.didom@gmail.com>
2025-06-02 18:35:38 +0200

    ✨ connection to existing table

---
+++ src/frontend/apps/impress/src/features/grist/useListGristTables.ts
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+
+import { APIError, errorCauses, gristFetchApi } from '@/api';
+
+export interface TableDescription {
+  tables: Table[];
+}
+
+export interface Table {
+  id: string;
+  fields: Fields;
+}
+
+export interface Fields {
+  tableRef: number;
+  onDemand: boolean;
+}
+
+export const useListGristTables = (
+  documentId?: number,
+): { tables: Table[] | null } => {
+  const [tables, setTables] = useState<Table[] | null>(null);
+
+  useEffect(() => {
+    const fetchTables = async () => {
+      if (!documentId) {
+        console.warn('Document ID is required to fetch Grist tables');
+        return;
+      }
+      const url = `docs/${documentId}/tables`;
+      const response = await gristFetchApi(url);
+      if (!response.ok) {
+        throw new APIError(
+          'Failed to fetch Grist tables',
+          await errorCauses(response),
+        );
+      }
+      return (await response.json()) as Promise<TableDescription>; // Adjusted to return an array of Table objects
+    };
+    fetchTables()
+      .then((response) => {
+        if (response) {
+          setTables(response.tables);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching Grist documents:', error);
+      });
+  }, [documentId]);
+
+  return {
+    tables,
+  };
+};
