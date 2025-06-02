d7c2cec7897cf235dc63736671e40a8952bea694
Simon Ser <contact@emersion.fr>
2025-06-02 19:36:33 +0200

    ✨(backend) add blocks converter to y-provider

--- src/frontend/servers/y-provider/src/routes.ts
+++ src/frontend/servers/y-provider/src/routes.ts
@@ -2,5 +2,6 @@ export const routes = {
   COLLABORATION_WS: '/collaboration/ws/',
   COLLABORATION_RESET_CONNECTIONS: '/collaboration/api/reset-connections/',
   CONVERT: '/api/convert/',
+  CONVERT_BLOCKS: '/api/convert-blocks/',
   COLLABORATION_GET_CONNECTIONS: '/collaboration/api/get-connections/',
 };
