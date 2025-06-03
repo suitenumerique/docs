cd7cbbb39d500b00aa766fee2d80b9f8caa55185
anaisberg <anais.schlienger@ext.bpifrance.fr>
2025-06-03 11:15:03 +0200

    💄 add button style

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/DatabaseBlock/AddRowButton.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/DatabaseBlock/AddRowButton.tsx
@@ -3,10 +3,23 @@ import { CustomCellRendererProps } from 'ag-grid-react';
 
 export const AddRowButton = (props: CustomCellRendererProps) => {
   console.log('AddRowButton props', props);
+  const color = '#817E77';
   return (
     <Button
       color="tertiary-text"
-      icon={<span className="material-icons">add</span>}
+      icon={
+        <span style={{ color }} className="material-icons">
+          add
+        </span>
+      }
+      style={{
+        color,
+        fontSize: '12px',
+        fontWeight: '400',
+        left: '-12px',
+        padding: 0,
+        width: '100%',
+      }}
     >
       new row
     </Button>
