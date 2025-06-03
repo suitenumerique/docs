ac70399aeecbaad01aca61a21f74f0ad4b31d83e
Clément Di Domenico <clement.didom@gmail.com>
2025-06-03 12:16:43 +0200

    ✨ create table when instanciating db block

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/DatabaseSelector.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/DatabaseSelector.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@openfun/cunningham-react';
 import React from 'react';
 
 import { Box, Text } from '@/components';
-import { Button } from '@openfun/cunningham-react';
 import { DatabaseSourceSelector } from '@/docs/doc-editor/components/DatabaseSourceSelector';
+import { useGristCreateDocAndTable } from '@/features/grist/useGristCreateTable';
+
+import { useDocStore } from '../../doc-management';
 
 type DatabaseSelectorProps = {
   onDatabaseSelected: (args: { documentId: string; tableId: string }) => void;
@@ -10,18 +13,39 @@ type DatabaseSelectorProps = {
 
 export const DatabaseSelector = ({
   onDatabaseSelected,
-}: DatabaseSelectorProps) => (
-  <Box
-    style={{
-      flexDirection: 'column',
-      gap: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-      width: '100%',
-    }}
-  >
-    <Button>Créer une nouvelle base de données vide</Button>
-    <Text>ou</Text>
-    <DatabaseSourceSelector onSourceSelected={onDatabaseSelected} />
-  </Box>
-);
+}: DatabaseSelectorProps) => {
+  const { createTable } = useGristCreateDocAndTable();
+  const { currentDoc } = useDocStore();
+
+  const handleCreateNewDatabase = () => {
+    if (!currentDoc) {
+      console.error('No current document found to create a new database.');
+      return;
+    }
+    createTable(currentDoc.title ?? currentDoc.id)
+      .then(({ documentId, tableId }) => {
+        onDatabaseSelected({ documentId, tableId });
+      })
+      .catch((error) => {
+        console.error('Error creating new database:', error);
+      });
+  };
+
+  return (
+    <Box
+      style={{
+        flexDirection: 'column',
+        gap: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+      }}
+    >
+      <Button onClick={handleCreateNewDatabase}>
+        Créer une nouvelle base de données vide
+      </Button>
+      <Text>ou</Text>
+      <DatabaseSourceSelector onSourceSelected={onDatabaseSelected} />
+    </Box>
+  );
+};
