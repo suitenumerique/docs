42f31cb9bcf9a3ae11ed3ca8338ecdba3398a999
Clément Di Domenico <clement.didom@gmail.com>
2025-06-03 16:10:39 +0200

    💄 revamp db source selector

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/DatabaseSelector.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/DatabaseSelector.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@openfun/cunningham-react';
-import React from 'react';
+import { Button, Radio, RadioGroup } from '@openfun/cunningham-react';
+import React, { useState } from 'react';
 
-import { Box, Text } from '@/components';
+import { Box, Icon, Text } from '@/components';
 import { DatabaseSourceSelector } from '@/docs/doc-editor/components/DatabaseSourceSelector';
 import { useGristCreateDocAndTable } from '@/features/grist/useGristCreateTable';
 import { useDocStore } from '../../doc-management';
+import styled from 'styled-components';
 
 type DatabaseSelectorProps = {
   onDatabaseSelected: (params: { documentId: string; tableId: string }) => void;
@@ -31,20 +32,87 @@ export const DatabaseSelector = ({
   };
 
   return (
-    <Box
-      style={{
-        flexDirection: 'column',
-        gap: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-      }}
-    >
-      <Button onClick={handleCreateNewDatabase}>
-        Créer une nouvelle base de données vide
-      </Button>
-      <Text>ou</Text>
-      <DatabaseSourceSelector onSourceSelected={onDatabaseSelected} />
-    </Box>
+    <Wrapper>
+      <Box
+        style={{
+          width: 40,
+          height: 40,
+          borderRadius: 20,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'rgb(133, 184, 255)',
+        }}
+      >
+        <Icon iconName="storage" color="rgb(62, 152, 255)" />
+      </Box>
+      <Title>Source de données</Title>
+      <Description>Choisissez votre méthode de création</Description>
+      <OptionsWrapper>
+        <Option>
+          <Box
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <Box>
+              <OptionTitle>Créer une nouvelle base de données vide</OptionTitle>
+              <Description>Partir d'une base de données vierge</Description>
+            </Box>
+            <Button
+              onClick={handleCreateNewDatabase}
+              icon={<Icon iconName="add" $color="white" />}
+            ></Button>
+          </Box>
+        </Option>
+        <Text style={{ fontWeight: 600, fontSize: 14 }}>ou</Text>
+        <Option>
+          <Box style={{ marginBottom: 10 }}>
+            <OptionTitle>
+              Sélectionner une base de données existante
+            </OptionTitle>
+            <Description>
+              Connecter une base de données Grist déjà créée
+            </Description>
+          </Box>
+          <DatabaseSourceSelector onSourceSelected={onDatabaseSelected} />
+        </Option>
+      </OptionsWrapper>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(Box)`
+  border: 2px solid rgb(160, 207, 255);
+  background-color: rgb(230, 243, 255);
+  border-radius: 4px;
+  width: 100%;
+  padding: 16px;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled(Text)`
+  font-weight: 800;
+  font-size: 18px;
+`;
+
+const Description = styled(Text)`
+  color: rgb(110, 110, 110);
+  font-size: 14px;
+`;
+
+const OptionTitle = styled(Text)`
+  font-weight: 600;
+  font-size: 14px;
+`;
+
+const Option = styled(Box)`
+  width: 100%;
+  border: 1px solid rgb(180, 180, 180);
+  border-radius: 4px;
+  padding: 8px 16px;
+`;
+
+const OptionsWrapper = styled(Box)`
+  width: 100%;
+  gap: 5px;
+  align-items: center;
+`;
