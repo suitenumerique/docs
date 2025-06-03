ecfa3aff13a364bc11bf868b99f46fb488f1f5f2
anaisberg <anais.schlienger@ext.bpifrance.fr>
2025-06-03 16:33:39 +0200

    ✨ use local grist api key

--- src/frontend/apps/impress/src/api/gristApi.ts
+++ src/frontend/apps/impress/src/api/gristApi.ts
@@ -2,7 +2,8 @@ import { gristApiUrl } from './config';
 
 export const gristFetchApi = async (input: string, init?: RequestInit) => {
   const apiUrl = `${gristApiUrl()}${input}`;
-  const bearerToken = `Bearer ${process.env.NEXT_PUBLIC_GRIST_API_KEY}`;
+  const apiKey = localStorage.getItem('grist_api_key');
+  const bearerToken = `Bearer ${apiKey}`;
 
   const headers = {
     'Content-Type': 'application/json',
