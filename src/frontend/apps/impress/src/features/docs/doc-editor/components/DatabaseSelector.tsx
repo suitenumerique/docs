93c1790425112facf55281f5906d92c8f0620d4c
Matthieu de Canteloube <matts2cant@gmail.com>
2025-06-03 11:09:24 +0200

    Add database selector

---
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/DatabaseSelector.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Box, Text } from '@/components';
+import {Button} from "@openfun/cunningham-react";
+import {DatabaseSourceSelector} from "@/docs/doc-editor/components/DatabaseSourceSelector";
+
+type DatabaseSelectorProps = {
+  onDatabaseSelected: (params: { documentId: string; tableId: string }) => void;
+};
+
+export const DatabaseSelector = ({ onDatabaseSelected }: DatabaseSelectorProps) => (
+  <Box
+    style={{
+      flexDirection: 'column',
+      gap: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+    }}
+  >
+    <Button
+      onClick={() => {
+        console.log('coucou');
+      }}
+    >
+      Créer une nouvelle base de données vide
+    </Button>
+    <Text>ou</Text>
+    <DatabaseSourceSelector
+      onSourceSelected={onDatabaseSelected}
+    />
+  </Box>
+);
