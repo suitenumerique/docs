7157dd41bd596df18f6863efafac34bad12cb635
Manuel Raynaud <manu@raynaud.io>
2025-06-12 11:08:23 +0200

    ✨(back) manage streaming with the ai service
    We want to handle both streaming or not when interacting with the AI
    backend service.

--- src/backend/core/tests/test_services_ai_services.py
+++ src/backend/core/tests/test_services_ai_services.py
@@ -15,6 +15,15 @@ from core.services.ai_services import AIService
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture(autouse=True)
+def ai_settings(settings):
+    """Fixture to set AI settings."""
+    settings.AI_MODEL = "llama"
+    settings.AI_BASE_URL = "http://example.com"
+    settings.AI_API_KEY = "test-key"
+    settings.AI_FEATURE_ENABLED = True
+
+
 @pytest.mark.parametrize(
     "setting_name, setting_value",
     [
@@ -23,22 +32,22 @@ pytestmark = pytest.mark.django_db
         ("AI_MODEL", None),
     ],
 )
-def test_api_ai_setting_missing(setting_name, setting_value):
+def test_services_ai_setting_missing(setting_name, setting_value, settings):
     """Setting should be set"""
+    setattr(settings, setting_name, setting_value)
 
-    with override_settings(**{setting_name: setting_value}):
-        with pytest.raises(
-            ImproperlyConfigured,
-            match="AI configuration not set",
-        ):
-            AIService()
+    with pytest.raises(
+        ImproperlyConfigured,
+        match="AI configuration not set",
+    ):
+        AIService()
 
 
 @override_settings(
     AI_BASE_URL="http://example.com", AI_API_KEY="test-key", AI_MODEL="test-model"
 )
 @patch("openai.resources.chat.completions.Completions.create")
-def test_api_ai__client_error(mock_create):
+def test_services_ai_client_error(mock_create):
     """Fail when the client raises an error"""
 
     mock_create.side_effect = OpenAIError("Mocked client error")
@@ -50,11 +59,24 @@ def test_api_ai__client_error(mock_create):
         AIService().transform("hello", "prompt")
 
 
+@patch("openai.resources.chat.completions.Completions.create")
+def test_services_ai_proxy_client_error(mock_create):
+    """Fail when the client raises an error"""
+
+    mock_create.side_effect = OpenAIError("Mocked client error")
+
+    with pytest.raises(
+        RuntimeError,
+        match="Failed to proxy AI request: Mocked client error",
+    ):
+        AIService().proxy({"messages": [{"role": "user", "content": "hello"}]})
+
+
 @override_settings(
     AI_BASE_URL="http://example.com", AI_API_KEY="test-key", AI_MODEL="test-model"
 )
 @patch("openai.resources.chat.completions.Completions.create")
-def test_api_ai__client_invalid_response(mock_create):
+def test_services_ai_client_invalid_response(mock_create):
     """Fail when the client response is invalid"""
 
     mock_create.return_value = MagicMock(
@@ -68,11 +90,50 @@ def test_api_ai__client_invalid_response(mock_create):
         AIService().transform("hello", "prompt")
 
 
+@patch("openai.resources.chat.completions.Completions.create")
+def test_services_ai_proxy_success(mock_create):
+    """The AI request should work as expect when called with valid arguments."""
+
+    mock_create.return_value = {
+        "id": "chatcmpl-test",
+        "object": "chat.completion",
+        "created": 1234567890,
+        "model": "test-model",
+        "choices": [
+            {
+                "index": 0,
+                "message": {"role": "assistant", "content": "Salut"},
+                "finish_reason": "stop",
+            }
+        ],
+    }
+
+    response = AIService().proxy({"messages": [{"role": "user", "content": "hello"}]})
+
+    expected_response = {
+        "id": "chatcmpl-test",
+        "object": "chat.completion",
+        "created": 1234567890,
+        "model": "test-model",
+        "choices": [
+            {
+                "index": 0,
+                "message": {"role": "assistant", "content": "Salut"},
+                "finish_reason": "stop",
+            }
+        ],
+    }
+    assert response == expected_response
+    mock_create.assert_called_once_with(
+        messages=[{"role": "user", "content": "hello"}], stream=False
+    )
+
+
 @override_settings(
     AI_BASE_URL="http://example.com", AI_API_KEY="test-key", AI_MODEL="test-model"
 )
 @patch("openai.resources.chat.completions.Completions.create")
-def test_api_ai__success(mock_create):
+def test_services_ai_success(mock_create):
     """The AI request should work as expect when called with valid arguments."""
 
     mock_create.return_value = MagicMock(
@@ -82,3 +143,44 @@ def test_api_ai__success(mock_create):
     response = AIService().transform("hello", "prompt")
 
     assert response == {"answer": "Salut"}
+
+
+@patch("openai.resources.chat.completions.Completions.create")
+def test_services_ai_proxy_with_stream(mock_create):
+    """The AI request should work as expect when called with valid arguments."""
+
+    mock_create.return_value = {
+        "id": "chatcmpl-test",
+        "object": "chat.completion",
+        "created": 1234567890,
+        "model": "test-model",
+        "choices": [
+            {
+                "index": 0,
+                "message": {"role": "assistant", "content": "Salut"},
+                "finish_reason": "stop",
+            }
+        ],
+    }
+
+    response = AIService().proxy(
+        {"messages": [{"role": "user", "content": "hello"}]}, stream=True
+    )
+
+    expected_response = {
+        "id": "chatcmpl-test",
+        "object": "chat.completion",
+        "created": 1234567890,
+        "model": "test-model",
+        "choices": [
+            {
+                "index": 0,
+                "message": {"role": "assistant", "content": "Salut"},
+                "finish_reason": "stop",
+            }
+        ],
+    }
+    assert response == expected_response
+    mock_create.assert_called_once_with(
+        messages=[{"role": "user", "content": "hello"}], stream=True
+    )
