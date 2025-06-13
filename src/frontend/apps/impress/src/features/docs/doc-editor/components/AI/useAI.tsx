f3370c8ef1df90d3709017c4591f150e97b3b412
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2025-06-13 11:31:21 +0200

    fixup! ⚡️(frontend) improve prompt of some actions

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/AI/useAI.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/AI/useAI.tsx
@@ -1,5 +1,4 @@
 import { createOpenAI } from '@ai-sdk/openai';
-import { CoreMessage } from 'ai';
 import { useMemo } from 'react';
 
 import { fetchAPI } from '@/api';
@@ -7,60 +6,23 @@ import { useConfig } from '@/core';
 import { Doc } from '@/docs/doc-management';
 
 import { useModuleAI } from './useModuleAI';
+import { usePromptAI } from './usePromptAI';
 
-const systemPrompts: Record<
-  'add-assistant' | 'add-formatting' | 'add-edit-instruction',
-  CoreMessage
-> = {
-  'add-assistant': {
-    role: 'system',
-    content:
-      'You are an AI assistant that helps users to edit their documents. Answer the user prompt in markdown format.',
-  },
-  'add-formatting': {
-    role: 'system',
-    content: `Add formatting to the text to make it more readable.`,
-  },
-  'add-edit-instruction': {
-    role: 'system',
-    content: `Keep adding to the document, do not delete or modify existing blocks.`,
-  },
-};
-
-const userPrompts: Record<string, string> = {
-  'continue writing':
-    'Keep writing about the content send in the prompt, expanding on the ideas.',
-  'improve writing':
-    'Improve the writing of the selected text. Make it more professional and clear.',
-  summarize:
-    'Summarize the selected text into a concise paragraph. Add a small summarize title above the text.',
-};
-
-/**
- * Custom implementation of the PromptBuilder that allows for using predefined prompts.
- *
- * This extends the default HTML promptBuilder from BlockNote to support custom prompt templates.
- * Custom prompts can be invoked using the pattern !promptName in the AI input field.
- */
 export const useAI = (doc: Doc) => {
   const conf = useConfig().data;
   const modules = useModuleAI();
   const aiAllowed = !!(conf?.AI_FEATURE_ENABLED && doc.abilities?.ai_proxy);
+  const promptBuilder = usePromptAI();
 
   return useMemo(() => {
     if (!aiAllowed || !modules || !conf?.AI_MODEL) {
       return;
     }
 
-    const { llmFormats, createAIExtension, createBlockNoteAIClient } = modules;
-
-    const clientAI = createBlockNoteAIClient({
-      baseURL: ``,
-      apiKey: '',
-    });
+    const { createAIExtension, llmFormats } = modules;
 
     const openai = createOpenAI({
-      ...clientAI.getProviderSettings('openai'),
+      apiKey: '', // The API key will be set by the AI proxy
       fetch: (input, init) => {
         // Create a new headers object without the Authorization header
         const headers = new Headers(init?.headers);
@@ -78,83 +40,17 @@ export const useAI = (doc: Doc) => {
       stream: conf.AI_STREAM,
       model,
       agentCursor: conf.AI_BOT,
-      // Create a custom promptBuilder that extends the default one
-      promptBuilder: async (editor, opts): Promise<Array<CoreMessage>> => {
-        const defaultPromptBuilder = llmFormats.html.defaultPromptBuilder;
-        const isTransform = !!opts.selectedBlocks?.length;
-
-        // Try to catch the action
-        const customPromptMatch = opts.userPrompt.match(/^([^:]+)(?=[:]|$)/);
-
-        let modifiedOpts = opts;
-        if (customPromptMatch?.length) {
-          const promptKey = customPromptMatch[0].trim().toLowerCase();
-
-          if (userPrompts[promptKey]) {
-            modifiedOpts = {
-              ...opts,
-              userPrompt: userPrompts[promptKey],
-            };
-          }
-        }
-
-        let prompts = await defaultPromptBuilder(editor, modifiedOpts);
-
-        if (!isTransform) {
-          prompts = prompts.map((prompt) => {
-            if (!prompt.content || typeof prompt.content !== 'string') {
-              return prompt;
-            }
-
-            /**
-             * Fix a bug when the initial content is empty
-             * TODO: Remove this when the bug is fixed in BlockNote
-             */
-            if (prompt.content === '[]') {
-              editor.insertBlocks(
-                [
-                  {
-                    id: 'base-block',
-                    content: ' ',
-                    type: 'paragraph',
-                  },
-                ],
-                'initialBlockId',
-                'after',
-              );
-
-              prompt.content =
-                '[{\"id\":\"base-block$\",\"block\":\"<p></p>\"}]';
-              return prompt;
-            }
-
-            if (
-              prompt.content.includes(
-                "You're manipulating a text document using HTML blocks.",
-              )
-            ) {
-              prompt = systemPrompts['add-assistant'];
-              return prompt;
-            }
-
-            if (
-              prompt.content.includes(
-                'First, determine what part of the document the user is talking about.',
-              )
-            ) {
-              prompt = systemPrompts['add-edit-instruction'];
-            }
-
-            return prompt;
-          });
-
-          prompts.push(systemPrompts['add-formatting']);
-        }
-
-        return prompts;
-      },
+      promptBuilder: promptBuilder(llmFormats.html.defaultPromptBuilder),
     });
 
     return extension;
-  }, [aiAllowed, conf, doc.id, modules]);
+  }, [
+    aiAllowed,
+    conf?.AI_BOT,
+    conf?.AI_MODEL,
+    conf?.AI_STREAM,
+    doc.id,
+    modules,
+    promptBuilder,
+  ]);
 };
