Theodo-GovTech/lasuite-docs

🌐(frontend) embed-block add some fr translations

src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
@@ -185,8 +185,8 @@ export const IframeBlock = (
   return (
     <ResizableFileBlockWrapper
       {...(props as any)} // eslint-disable-line @typescript-eslint/no-explicit-any
-      buttonText="Embed"
-      buttonIcon={<Icon iconName="language" $size="18px" />}
+      buttonText="Add a callout block"
+      buttonIcon={<Icon iconName="link" $size="18px" />}
     >
       <IFrameViewer {...props} />
     </ResizableFileBlockWrapper>
@@ -211,17 +211,17 @@ export const getEmbedReactSlashMenuItems = (
         type: 'embed',
       });
     },
-    aliases: ['newEmbed', 'iframeBN', 'linkBN'],
+    aliases: ['embed', 'link', 'integration', 'lien'],
     group,
     icon: <Icon iconName="language" $size="18px" />,
-    subtext: t('Add an embed block'),
+    subtext: t('Add an embedded website'),
   },
 ];
 
 export const getEmbedFormattingToolbarItems = (
   t: TFunction<'translation', undefined>,
 ): BlockTypeSelectItem => ({
-  name: t('Embed'),
+  name: t('Add an embedded website'),
   type: 'embed',
   icon: () => <Icon iconName="link" $size="16px" />,
   isSelected: (block: { type: string }) => block.type === 'embed',

suitenumerique/docs

src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/EmbedBlock.tsx
@@ -174,8 +174,8 @@ export const IframeBlock = (
   return (
     <ResizableFileBlockWrapper
       {...(props as any)} // eslint-disable-line @typescript-eslint/no-explicit-any
-      buttonText="Embed"
-      buttonIcon={<Icon iconName="language" $size="18px" />}
+      buttonText="Add an embedded block"
+      buttonIcon={<Icon iconName="link" $size="18px" />}
     >
       <IFrameViewer {...props} />
     </ResizableFileBlockWrapper>
@@ -200,17 +200,17 @@ export const getEmbedReactSlashMenuItems = (
         type: 'embed',
       });
     },
-    aliases: ['newEmbed', 'iframeBN', 'linkBN'],
+    aliases: ['embed', 'link', 'integration', 'lien'],
     group,
     icon: <Icon iconName="language" $size="18px" />,
-    subtext: t('Add an embed block'),
+    subtext: t('Add an embedded website'),
   },
 ];
 
 export const getEmbedFormattingToolbarItems = (
   t: TFunction<'translation', undefined>,
 ): BlockTypeSelectItem => ({
-  name: t('Embed'),
+  name: t('Add an embedded website'),
   type: 'embed',
   icon: () => <Icon iconName="link" $size="16px" />,
   isSelected: (block: { type: string }) => block.type === 'embed',