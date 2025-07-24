b7ffc766d8022d73478a1465ad6b01daa3631317
Sylvain Zimmer <sylvain@sylvainzimmer.com>
2025-07-24 14:52:16 +0200

    🗑️(convert) cleanup old content route

--- src/frontend/servers/y-provider/src/routes.ts
+++ src/frontend/servers/y-provider/src/routes.ts
@@ -2,6 +2,5 @@ export const routes = {
   COLLABORATION_WS: '/collaboration/ws/',
   COLLABORATION_RESET_CONNECTIONS: '/collaboration/api/reset-connections/',
   CONVERT: '/api/convert/',
-  CONTENT: '/api/content/',
   COLLABORATION_GET_CONNECTIONS: '/collaboration/api/get-connections/',
 };
