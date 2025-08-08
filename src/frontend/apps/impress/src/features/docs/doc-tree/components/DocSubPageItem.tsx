23004ee1eb8760f93d2bc129806217988dc22b0e
Cyril <c.gromoff@gmail.com>
2025-08-08 14:24:39 +0200

    fixup! ⚡️(frontend) improve accessibility of selected document's sub-menu

--- src/frontend/apps/impress/src/features/docs/doc-tree/components/DocSubPageItem.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-tree/components/DocSubPageItem.tsx
@@ -9,10 +9,10 @@ import { useTranslation } from 'react-i18next';
 import { css } from 'styled-components';
 
 import { Box, BoxButton, Icon, Text } from '@/components';
-import { useActionableMode } from '@/features/docs/doc-tree/hooks/useActionableMode';
-import { useTreeItemKeyboardActivate } from '@/features/docs/doc-tree/hooks/useTreeItemKeyboardActivate';
 import { useCunninghamTheme } from '@/cunningham';
 import { Doc, useTrans } from '@/features/docs/doc-management';
+import { useActionableMode } from '@/features/docs/doc-tree/hooks/useActionableMode';
+import { useTreeItemKeyboardActivate } from '@/features/docs/doc-tree/hooks/useTreeItemKeyboardActivate';
 import { useLeftPanelStore } from '@/features/left-panel';
 import { useResponsiveStore } from '@/stores';
 
