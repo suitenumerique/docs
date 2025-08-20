e7a4a61c183cde9de49bb4eaa781306e2978bec0
dakshesh14 <65905942+dakshesh14@users.noreply.github.com>
2025-08-20 23:20:11 +0530

    ♻️(frontend) moved drag handle logic to a hook
    Moved drag handler logic to a hook to make component dumb.
    
    Signed-off-by: dakshesh14 <65905942+dakshesh14@users.noreply.github.com>

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/PdfBlock.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/custom-blocks/PdfBlock.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { insertOrUpdateBlock } from '@blocknote/core';
 import {
   AddFileButton,
@@ -5,15 +6,17 @@ import {
   createReactBlockSpec,
 } from '@blocknote/react';
 import { TFunction } from 'i18next';
-import { useTranslation } from 'react-i18next';
+import { useCallback } from 'react';
 
-import { Icon } from '@/components';
+import { Box, Icon } from '@/components';
+import { useResponsiveStore } from '@/stores';
 
+import { usePdfResizer } from '../../hook/usePdfResizer';
 import { DocsBlockNoteEditor } from '../../types';
 
 type FileBlockEditor = Parameters<typeof AddFileButton>[0]['editor'];
 
-export const PDFBlock = createReactBlockSpec(
+export const PdfBlock = createReactBlockSpec(
   {
     type: 'pdf',
     content: 'none',
@@ -28,9 +31,23 @@ export const PDFBlock = createReactBlockSpec(
   },
   {
     render: ({ editor, block, contentRef }) => {
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-      const { t } = useTranslation();
       const pdfUrl = block.props.url;
+
+      const { isMobile } = useResponsiveStore();
+
+      const handleResizeEnd = useCallback(
+        (width: number) => {
+          editor.updateBlock(block, {
+            props: { previewWidth: width },
+          });
+        },
+        [editor, block],
+      );
+
+      const { wrapperRef, pdfWidth, handlePointerDown } = usePdfResizer(
+        block.props.previewWidth ?? 100,
+        handleResizeEnd,
+      );
 
       return (
         <div ref={contentRef} className="bn-file-block-content-wrapper">
@@ -43,22 +60,71 @@ export const PDFBlock = createReactBlockSpec(
             />
           ) : (
             <FileBlockWrapper block={block} editor={editor as FileBlockEditor}>
-              <div style={{ width: '100%', height: '500px' }}>
-                <embed
-                  src={pdfUrl}
-                  type="application/pdf"
-                  width="100%"
-                  height="100%"
-                  contentEditable={false}
-                  draggable={false}
-                  onClick={() => editor.setTextCursorPosition(block)}
-                  aria-label={block.props.name}
-                />
-                <p>
-                  {t('Your browser does not support PDFs.')}{' '}
-                  <a href={pdfUrl}>{t('Download the pdf instead.')}</a>
-                </p>
-              </div>
+              {isMobile ? (
+                <Box
+                  $display="flex"
+                  $align="center"
+                  $justify="center"
+                  $direction="row"
+                  $gap="1rem"
+                >
+                  <Icon iconName="picture_as_pdf" $size="18px" />
+                  <Box
+                    as="p"
+                    $display="inline-block"
+                    style={{
+                      textOverflow: 'ellipsis',
+                      whiteSpace: 'nowrap',
+                      overflow: 'hidden',
+                      display: 'inline',
+                    }}
+                  >
+                    {block.props.name}
+                  </Box>
+                </Box>
+              ) : (
+                <Box ref={wrapperRef} $width="100%" $position="relative">
+                  <Box
+                    $width={`${pdfWidth}%`}
+                    $height="500px"
+                    $position="relative"
+                    $css={`
+                    border: 1px solid #ccc;
+                    margin: auto;
+                  `}
+                  >
+                    <embed
+                      src={pdfUrl}
+                      type="application/pdf"
+                      width="100%"
+                      height="100%"
+                      contentEditable={false}
+                      draggable={false}
+                      onClick={() => editor.setTextCursorPosition(block)}
+                    />
+                    {/* Right-edge drag handle */}
+                    <div
+                      role="separator"
+                      aria-orientation="vertical"
+                      style={{
+                        position: 'absolute',
+                        top: '50%',
+                        right: -4,
+                        width: 4,
+                        height: '7.5rem',
+                        borderRadius: '2px',
+                        background: 'gray',
+                        cursor: 'ew-resize',
+                        zIndex: 1,
+                        touchAction: 'none',
+                        transform: 'translateY(-50%)',
+                      }}
+                      onPointerDown={handlePointerDown}
+                      onDragStart={(e) => e.preventDefault()}
+                    />
+                  </Box>
+                </Box>
+              )}
             </FileBlockWrapper>
           )}
         </div>
