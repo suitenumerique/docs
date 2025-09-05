Mu-L/docs-1

✨(frontend) make delete buttons nvda-accessible
add aria-labels and include close button in title prop so NVDA announces actions

Signed-off-by: Cyril <c.gromoff@gmail.com>

src/frontend/apps/impress/src/features/docs/doc-share/components/DocShareModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalSize } from '@openfun/cunningham-react';
+import { Button, Modal, ModalSize } from '@openfun/cunningham-react';
 import { useMemo, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { createGlobalStyle, css } from 'styled-components';
@@ -137,12 +137,29 @@ export const DocShareModal = ({ doc, onClose, isRootDoc = true }: Props) => {
         aria-label={t('Share modal')}
         size={isDesktop ? ModalSize.LARGE : ModalSize.FULL}
         onClose={onClose}
-        title={<Box $align="flex-start">{t('Share the document')}</Box>}
+        title={
+          <Box $direction="row" $justify="space-between" $align="center">
+            <Box $align="flex-start">{t('Share the document')}</Box>
+            <Button
+              aria-label={t('Close the modal')}
+              size="small"
+              color="primary-text"
+              onClick={onClose}
+              icon={
+                <Box aria-hidden="true" className="material-icons-filled">
+                  close
+                </Box>
+              }
+            />
+          </Box>
+        }
+        hideCloseButton
       >
         <ShareModalStyle />
         <Box
           aria-label={t('Share modal')}
-          $height={canViewAccesses ? modalContentHeight : 'auto'}
+          $height="auto"
+          $maxHeight={canViewAccesses ? modalContentHeight : 'none'}
           $overflow="hidden"
           className="--docs--doc-share-modal noPadding "
           $justify="space-between"

suitenumerique/docs

src/frontend/apps/impress/src/features/docs/doc-share/components/DocShareModal.tsx
@@ -5,6 +5,7 @@ import { createGlobalStyle, css } from 'styled-components';
 import { useDebouncedCallback } from 'use-debounce';
 
 import { Box, HorizontalSeparator, Text } from '@/components';
+import ButtonCloseModal from '@/components/modal/ButtonCloseModal';
 import {
   QuickSearch,
   QuickSearchData,
@@ -137,12 +138,23 @@ export const DocShareModal = ({ doc, onClose, isRootDoc = true }: Props) => {
         aria-label={t('Share modal')}
         size={isDesktop ? ModalSize.LARGE : ModalSize.FULL}
         onClose={onClose}
-        title={<Box $align="flex-start">{t('Share the document')}</Box>}
+        title={
+          <Box $direction="row" $justify="space-between" $align="center">
+            <Box $align="flex-start">{t('Share the document')}</Box>
+            <ButtonCloseModal
+              aria-label={t('Close the share modal')}
+              onClick={onClose}
+            />
+          </Box>
+        }
+        hideCloseButton
       >
         <ShareModalStyle />
         <Box
-          aria-label={t('Share modal')}
-          $height={canViewAccesses ? modalContentHeight : 'auto'}
+          role="dialog"
+          aria-label={t('Share modal content')}
+          $height="auto"
+          $maxHeight={canViewAccesses ? modalContentHeight : 'none'}
           $overflow="hidden"
           className="--docs--doc-share-modal noPadding "
           $justify="space-between"