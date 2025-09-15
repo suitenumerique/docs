f39463d41c0b4ee57d5c824b99f145b0ed66c1cc
Olivier Laurendeau <o.laurendeau@gmail.com>
2025-09-15 15:26:55 +0200

    ✨(frontend) add an EmojiPicker in the document tree
    This allows users to easily add emojis easily to
    their documents from the tree, enhancing the
    overall user experience.

--- src/frontend/apps/impress/src/features/docs/doc-editor/components/index.ts
+++ src/frontend/apps/impress/src/features/docs/doc-editor/components/index.ts
@@ -1,2 +1,3 @@
 export * from './DocEditor';
+export * from './EmojiPicker';
 export * from './custom-blocks/';
--- src/frontend/apps/impress/src/features/docs/doc-tree/components/DocSubPageItem.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-tree/components/DocSubPageItem.tsx
@@ -164,7 +164,14 @@ export const DocSubPageItem = (props: TreeViewNodeProps<Doc>) => {
           `}
         >
           <Box $width="16px" $height="16px">
-            <DocIcon emoji={emoji} defaultIcon={<SubPageIcon />} $size="sm" />
+            <DocIcon
+              emoji={emoji}
+              defaultIcon={<SubPageIcon />}
+              $size="sm"
+              emojiPicker
+              docId={doc.id}
+              title={doc.title}
+            />
           </Box>
 
           <Box
