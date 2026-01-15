a5111809bb402d40fbc0edcdc06cc54a604f7837
Anthony LC <anthony.le-courric@mail.numerique.gouv.fr>
2026-01-15 16:30:47 +0100

    ✨(frontend) add email validation pages

---
+++ src/frontend/apps/impress/src/pages/user_reconciliations/inactive/[id]/index.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { UserReconciliation } from '@/features/auth/components/UserReconciliation';
+import { PageLayout } from '@/layouts';
+import { NextPageWithLayout } from '@/types/next';
+
+const Page: NextPageWithLayout = () => {
+  const { t } = useTranslation();
+  const {
+    query: { id },
+  } = useRouter();
+
+  if (typeof id !== 'string') {
+    return null;
+  }
+
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+        <title>{`${t('User reconciliation')} - ${t('Docs')}`}</title>
+        <meta
+          property="og:title"
+          content={`${t('User reconciliation')} - ${t('Docs')}`}
+          key="title"
+        />
+      </Head>
+      <UserReconciliation type="inactive" reconciliationId={id} />
+    </>
+  );
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <PageLayout withFooter={false}>{page}</PageLayout>;
+};
+
+export default Page;
