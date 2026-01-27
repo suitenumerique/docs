e23389a22fc45ec40427282c5445db6d93478a7e
charles <charles.englebert@protonmail.com>
2026-01-27 16:38:43 +0100

    ✨(frontend) handle sub-docs
    handle filtering on sub-docs

--- src/frontend/apps/impress/src/features/docs/doc-management/api/searchDocs.tsx
+++ src/frontend/apps/impress/src/features/docs/doc-management/api/searchDocs.tsx
@@ -2,28 +2,42 @@ import { useQuery } from '@tanstack/react-query';
 
 import { APIError, APIList, errorCauses, fetchAPI } from '@/api';
 import { KEY_LIST_DOC } from '@/docs/doc-management';
+import { DocSearchTarget } from '@/docs/doc-search';
 
 import { Doc } from '../types';
 
 export type DocsParams = {
   q: string;
+  target?: DocSearchTarget;
+  parentPath?: string;
 };
 
-export const constructParams = (params: DocsParams): URLSearchParams => {
+export const constructParams = ({
+  q,
+  target,
+  parentPath,
+}: DocsParams): URLSearchParams => {
   const searchParams = new URLSearchParams();
 
-  if (params.q.length > 0) {
-    searchParams.set('q', params.q);
+  if (q.length > 0) {
+    searchParams.set('q', q);
   } else {
     searchParams.set('q', '*');
+  }
+  if (target === DocSearchTarget.CURRENT && parentPath) {
+    searchParams.set('path', parentPath);
   }
 
   return searchParams;
 };
 
 export type DocsResponse = APIList<Doc>;
-export const getDocs = async (params: DocsParams): Promise<DocsResponse> => {
-  const searchParams = constructParams(params);
+export const getDocs = async ({
+  q,
+  target,
+  parentPath,
+}: DocsParams): Promise<DocsResponse> => {
+  const searchParams = constructParams({ q, target, parentPath });
   const response = await fetchAPI(
     `documents/search/?${searchParams.toString()}`,
   );
@@ -35,9 +49,9 @@ export const getDocs = async (params: DocsParams): Promise<DocsResponse> => {
   return response.json() as Promise<DocsResponse>;
 };
 
-export const useSearchDocs = (params: DocsParams) => {
+export const useSearchDocs = ({ q, target, parentPath }: DocsParams) => {
   return useQuery<DocsResponse, APIError, DocsResponse>({
-    queryKey: [KEY_LIST_DOC, 'search', params],
-    queryFn: () => getDocs(params),
+    queryKey: [KEY_LIST_DOC, 'search', { q, target, parentPath }],
+    queryFn: () => getDocs({ q, target, parentPath }),
   });
 };
