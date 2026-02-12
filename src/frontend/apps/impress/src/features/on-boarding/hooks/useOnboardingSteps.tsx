92d9961672901538c48bc6e3829a30be70c8db49
Cyril <c.gromoff@gmail.com>
2026-02-12 12:53:39 +0100

    fixup! ✨(frontend) add onboarding modal with help menu button

--- src/frontend/apps/impress/src/features/on-boarding/hooks/useOnboardingSteps.tsx
+++ src/frontend/apps/impress/src/features/on-boarding/hooks/useOnboardingSteps.tsx
@@ -23,46 +23,25 @@ export const useOnboardingSteps = () => {
     contextualTokens.content.semantic.brand.tertiary ??
     colorsTokens['brand-600'];
 
-  const step1Title = t('Compose your doc easily');
-  const step1Description = t(
-    'Move, duplicate, and transform your texts, headings, lists, images without breaking your layout.',
-  );
-  const step2Title = t('Format your content with the toolbar');
-  const step2Description = t(
-    'Apply styles, structure, and emphasis in one click—keep documents clean, consistent, and easy to scan.',
-  );
-  const step3Title = t('Share and collaborate with ease');
-  const step3Description = t(
-    'Decide exactly who can view, comment, edit—or simply use shareable links.',
-  );
-  const step4Title = t('Draw inspiration from the content library');
-  const step4Description = t(
-    'Start from ready-made templates for common use cases, then customize them to match your workflow in minutes.',
-  );
-  const step1ImageSrc = t('assets/onboarding/step_1', {
-    desc: 'URL of onboarding step 1 preview image',
-    defaultValue: '/assets/on-boarding/step_1_FR.gif',
-  });
-  const step2ImageSrc = t('assets/onboarding/step_2', {
-    desc: 'URL of onboarding step 2 preview image',
-    defaultValue: '/assets/on-boarding/step_2_FR.gif',
-  });
-  const step3ImageSrc = t('assets/onboarding/step_3', {
-    desc: 'URL of onboarding step 3 preview image',
-    defaultValue: '/assets/on-boarding/step_3_FR.png',
-  });
-  const step4ImageSrc = t('assets/onboarding/step_4', {
-    desc: 'URL of onboarding step 4 preview image',
-    defaultValue: '/assets/on-boarding/step_4_FR.png',
-  });
-
   return useMemo<OnboardingStepsData>(
     () => ({
       imageSources: [
-        step1ImageSrc,
-        step2ImageSrc,
-        step3ImageSrc,
-        step4ImageSrc,
+        t('src_img_onboarding_step_1', {
+          desc: 'URL of onboarding step 1 preview image',
+          defaultValue: '/assets/on-boarding/step_1_EN.gif',
+        }),
+        t('src_img_onboarding_step_2', {
+          desc: 'URL of onboarding step 2 preview image',
+          defaultValue: '/assets/on-boarding/step_2_EN.gif',
+        }),
+        t('src_img_onboarding_step_3', {
+          desc: 'URL of onboarding step 3 preview image',
+          defaultValue: '/assets/on-boarding/step_3_EN.png',
+        }),
+        t('src_img_onboarding_step_4', {
+          desc: 'URL of onboarding step 4 preview image',
+          defaultValue: '/assets/on-boarding/step_4_EN.png',
+        }),
       ],
       steps: [
         {
@@ -76,9 +55,19 @@ export const useOnboardingSteps = () => {
               <DragIndicatorIcon aria-hidden="true" />
             </OnboardingStepIcon>
           ),
-          title: step1Title,
-          description: step1Description,
-          content: <OnboardingStepImage src={step1ImageSrc} alt={step1Title} />,
+          title: t('Compose your doc easily'),
+          description: t(
+            'Move, duplicate, and transform your texts, headings, lists, images without breaking your layout.',
+          ),
+          content: (
+            <OnboardingStepImage
+              src={t('src_img_onboarding_step_1', {
+                desc: 'URL of onboarding step 1 preview image',
+                defaultValue: '/assets/on-boarding/step_1_EN.gif',
+              })}
+              alt={t('Compose your doc easily')}
+            />
+          ),
         },
         {
           icon: (
@@ -91,9 +80,19 @@ export const useOnboardingSteps = () => {
               <FormatTextIcon aria-hidden="true" />
             </OnboardingStepIcon>
           ),
-          title: step2Title,
-          description: step2Description,
-          content: <OnboardingStepImage src={step2ImageSrc} alt={step2Title} />,
+          title: t('Format your content with the toolbar'),
+          description: t(
+            'Apply styles, structure, and emphasis in one click—keep documents clean, consistent, and easy to scan.',
+          ),
+          content: (
+            <OnboardingStepImage
+              src={t('src_img_onboarding_step_2', {
+                desc: 'URL of onboarding step 2 preview image',
+                defaultValue: '/assets/on-boarding/step_2_EN.gif',
+              })}
+              alt={t('Format your content with the toolbar')}
+            />
+          ),
         },
         {
           icon: (
@@ -106,9 +105,19 @@ export const useOnboardingSteps = () => {
               <FileShareIcon aria-hidden="true" />
             </OnboardingStepIcon>
           ),
-          title: step3Title,
-          description: step3Description,
-          content: <OnboardingStepImage src={step3ImageSrc} alt={step3Title} />,
+          title: t('Share and collaborate with ease'),
+          description: t(
+            'Decide exactly who can view, comment, edit—or simply use shareable links.',
+          ),
+          content: (
+            <OnboardingStepImage
+              src={t('src_img_onboarding_step_3', {
+                desc: 'URL of onboarding step 3 preview image',
+                defaultValue: '/assets/on-boarding/step_3_EN.png',
+              })}
+              alt={t('Share and collaborate with ease')}
+            />
+          ),
         },
         {
           icon: (
@@ -121,26 +130,22 @@ export const useOnboardingSteps = () => {
               <StackTemplateIcon aria-hidden="true" />
             </OnboardingStepIcon>
           ),
-          title: step4Title,
-          description: step4Description,
-          content: <OnboardingStepImage src={step4ImageSrc} alt={step4Title} />,
+          title: t('Draw inspiration from the content library'),
+          description: t(
+            'Start from ready-made templates for common use cases, then customize them to match your workflow in minutes.',
+          ),
+          content: (
+            <OnboardingStepImage
+              src={t('src_img_onboarding_step_4', {
+                desc: 'URL of onboarding step 4 preview image',
+                defaultValue: '/assets/on-boarding/step_4_EN.png',
+              })}
+              alt={t('Draw inspiration from the content library')}
+            />
+          ),
         },
       ],
     }),
-    [
-      activeColor,
-      step1ImageSrc,
-      step1Description,
-      step1Title,
-      step2ImageSrc,
-      step2Description,
-      step2Title,
-      step3ImageSrc,
-      step3Description,
-      step3Title,
-      step4ImageSrc,
-      step4Description,
-      step4Title,
-    ],
+    [activeColor, t],
   );
 };
