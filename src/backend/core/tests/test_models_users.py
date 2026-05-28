a20ba1a78efcb6a9ad3ac8bbf70ec846f0cbce2f
Manuel Raynaud <manu@raynaud.io>
2026-05-28 08:50:40 +0200

    🐛(backend) use computed_link_reach in handle_onboarding_document
    In the model method User::_handle_onboarding_documents_access we do not
    allow using documents with restricted link_reach to be added as
    onboarding documents. To check the real link_reach of the document, we
    must use instead the computed_link_reach to be sure that a sub document
    can also be used and compute its correct link_reach.

--- src/backend/core/tests/test_models_users.py
+++ src/backend/core/tests/test_models_users.py
@@ -195,6 +195,25 @@ def test_models_users_handle_onboarding_documents_on_restricted_document_is_not_
     assert not models.LinkTrace.objects.filter(user=user, document=document).exists()
 
 
+def test_models_users_handle_onboarding_documents_computed_link_reach_not_restricted():
+    """Test that the computed_link_reach is used to check the real link_reach is used."""
+
+    parent = factories.DocumentFactory(link_reach=models.LinkReachChoices.PUBLIC)
+    document = factories.DocumentFactory(
+        parent=parent, link_reach=models.LinkReachChoices.RESTRICTED
+    )
+
+    assert document.computed_link_reach == models.LinkReachChoices.PUBLIC
+
+    with override_settings(USER_ONBOARDING_DOCUMENTS=[str(document.id)]):
+        user = factories.UserFactory()
+
+    assert models.LinkTrace.objects.filter(user=user, document=document).exists()
+    user_favorites = models.DocumentFavorite.objects.filter(user=user)
+    assert user_favorites.count() == 1
+    assert user_favorites.filter(document=document).exists()
+
+
 @override_settings(USER_ONBOARDING_SANDBOX_DOCUMENT=None)
 def test_models_users_duplicate_onboarding_sandbox_document_no_setting():
     """
