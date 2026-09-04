ArthurBlanchon/docs-ai-assistant

♻️(frontend) extract shared footer actions and bar
Footer actions lived in the left panel and could not be reused elsewhere.

src/frontend/apps/impress/src/features/footer/components/FooterActions.tsx
@@ -1,0 +1,58 @@
+import { UserMenu } from '@gouvfr-lasuite/ui-kit';
+import { useTranslation } from 'react-i18next';
+import { createGlobalStyle } from 'styled-components';
+
+import { Box } from '@/components';
+import { Waffle } from '@/components/Waffle';
+import { ButtonLogin, gotoLogout, useAuth } from '@/features/auth';
+import { HelpMenu } from '@/features/help';
+import { LanguagePicker } from '@/features/language/components/LanguagePicker';
+
+const FooterActionsGlobalStyle = createGlobalStyle`
+  .user-menu__actions .c__language-picker{
+    width: auto;
+  }
+`;
+
+type FooterActionsProps = {
+  /** In the left panel the help menu sits at the opposite edge of the row. */
+  isHelpMenuAside?: boolean;
+};
+
+export const FooterActions = ({
+  isHelpMenuAside = false,
+}: FooterActionsProps) => {
+  const { t } = useTranslation();
+  const { user } = useAuth();
+
+  const userMenu = user || {
+    full_name: t('Guest'),
+    email: '',
+  };
+
+  return (
+    <>
+      <FooterActionsGlobalStyle />
+      <Box
+        $padding={{ horizontal: 'sm' }}
+        $direction="row"
+        $align="center"
+        $gap="3xs"
+        $justify={isHelpMenuAside ? 'space-between' : undefined}
+        className="--docs--footer-actions"
+      >
+        <Box $direction="row" $align="center" $gap="3xs">
+          <UserMenu
+            user={userMenu}
+            logout={user ? gotoLogout : undefined}
+            actions={<LanguagePicker />}
+            withMobileView={false}
+          />
+          <Waffle />
+          <ButtonLogin />
+        </Box>
+        <HelpMenu />
+      </Box>
+    </>
+  );
+};

suitenumerique/docs

src/frontend/apps/impress/src/features/footer/components/FooterActions.tsx
@@ -1,0 +1,58 @@
+import { UserMenu } from '@gouvfr-lasuite/ui-components';
+import { useTranslation } from 'react-i18next';
+import { createGlobalStyle } from 'styled-components';
+
+import { Box } from '@/components';
+import { Waffle } from '@/components/Waffle';
+import { ButtonLogin, gotoLogout, useAuth } from '@/features/auth';
+import { HelpMenu } from '@/features/help';
+import { LanguagePicker } from '@/features/language/components/LanguagePicker';
+
+const FooterActionsGlobalStyle = createGlobalStyle`
+  .user-menu__actions .c__language-picker{
+    width: auto;
+  }
+`;
+
+type FooterActionsProps = {
+  /** In the left panel the help menu sits at the opposite edge of the row. */
+  isHelpMenuAside?: boolean;
+};
+
+export const FooterActions = ({
+  isHelpMenuAside = false,
+}: FooterActionsProps) => {
+  const { t } = useTranslation();
+  const { user } = useAuth();
+
+  const userMenu = user || {
+    full_name: t('Guest'),
+    email: '',
+  };
+
+  return (
+    <>
+      <FooterActionsGlobalStyle />
+      <Box
+        $padding={{ horizontal: 'sm' }}
+        $direction="row"
+        $align="center"
+        $gap="3xs"
+        $justify={isHelpMenuAside ? 'space-between' : undefined}
+        className="--docs--footer-actions"
+      >
+        <Box $direction="row" $align="center" $gap="3xs">
+          <UserMenu
+            user={userMenu}
+            logout={user ? gotoLogout : undefined}
+            actions={<LanguagePicker />}
+            withMobileView={false}
+          />
+          <Waffle />
+          <ButtonLogin />
+        </Box>
+        <HelpMenu />
+      </Box>
+    </>
+  );
+};